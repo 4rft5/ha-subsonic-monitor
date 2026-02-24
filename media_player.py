f113cdf80b60eb22a2baffb7569c17e99c9f1413
4rft5 <74219775+4rft5@users.noreply.github.com>
2026-02-23 21:08:04 -0700

    Added cover art caching, fix #2

--- media_player.py
+++ media_player.py
@@ -1,35 +1,33 @@
+import os
 import aiohttp
 from homeassistant.components.media_player import MediaPlayerEntity
 from homeassistant.components.media_player.const import MediaPlayerState
 from urllib.parse import quote_plus
 from .const import DOMAIN
 
-async def fetch_subsonic_users(username, password, server):
-    import urllib.parse
-    u = urllib.parse.quote_plus(username)
-    p = urllib.parse.quote_plus(password)
-    url = f"{server}/rest/getNowPlaying.view?u={u}&p={p}&v=1.16.1&f=json"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            if resp.status != 200:
-                return []
-            try:
-                data = await resp.json()
-                return data.get("subsonic-response", {}).get("nowPlaying", {}).get("entry", [])
-            except Exception:
-                return []
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    data = hass.data[DOMAIN][entry.entry_id]
+    username = data["username"]
+    password = data["password"]
+    server = data["server_url"]
+    player = SubsonicPlayer(username, password, server, hass)
+    async_add_entities([player])
+
 
 class SubsonicPlayer(MediaPlayerEntity):
-    def __init__(self, username, password, server):
+    def __init__(self, username, password, server, hass):
         self._attr_name = f"Subsonic - {username}"
         self._username = username
         self._password = password
         self._server = server
+        self._hass = hass
         self._state = MediaPlayerState.IDLE
         self._media_title = None
         self._media_artist = None
         self._media_album = None
         self._media_image_url = None
+        self._current_cover_id = None
         self._status = "Idle"
         self._icon = "mdi:stop"
 
@@ -46,7 +44,6 @@ class SubsonicPlayer(MediaPlayerEntity):
                     if resp.status != 200:
                         self._set_idle()
                         return
-
                     data = await resp.json()
             except Exception:
                 self._set_idle()
@@ -68,14 +65,31 @@ class SubsonicPlayer(MediaPlayerEntity):
         self._media_album = item.get("album")
 
         cover_tag = item.get("coverArt")
-        if cover_tag:
-            self._media_image_url = (
+        cover_id = item.get("id")
+
+        if cover_tag and cover_id != self._current_cover_id:
+            cover_url = (
                 f"{self._server}/rest/getCoverArt.view"
-                f"?id={item.get('id')}&tag={cover_tag}"
+                f"?id={cover_id}&tag={cover_tag}"
                 f"&u={quote_plus(self._username)}&p={quote_plus(self._password)}"
                 f"&c=ha-subsonic&v=1.16.1"
             )
-        else:
+            try:
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(cover_url) as img_resp:
+                        if img_resp.status == 200:
+                            img_bytes = await img_resp.read()
+                            www_path = self._hass.config.path("www")
+                            os.makedirs(www_path, exist_ok=True)
+                            img_filename = f"subsonic_cover_{self._username}.jpg"
+                            img_path = os.path.join(www_path, img_filename)
+                            with open(img_path, "wb") as f:
+                                f.write(img_bytes)
+                            self._current_cover_id = cover_id
+                            self._media_image_url = f"/local/{img_filename}?v={cover_id}"
+            except Exception:
+                self._media_image_url = None
+        elif not cover_tag:
             self._media_image_url = None
 
         self._state = MediaPlayerState.PLAYING
@@ -125,12 +139,3 @@ class SubsonicPlayer(MediaPlayerEntity):
             "Artist": self._media_artist,
             "Album": self._media_album,
         }
-
-
-async def async_setup_entry(hass, entry, async_add_entities):
-    data = hass.data[DOMAIN][entry.entry_id]
-    username = data["username"]
-    password = data["password"]
-    server = data["server_url"]
-    player = SubsonicPlayer(username, password, server)
-    async_add_entities([player])
